04cfaeee27713ebcff572b749d369ca3d3552e92
KB01111 <ralleyhainne@gmail.com>
2026-03-28 04:55:27 +0100

    Initial commit

---
+++ .electron-builder.js
@@ -0,0 +1,55 @@
+const { name, productName, appId, version } = require('./package.json');
+const icon = require('./lib/icon.js')('build');
+
+const fileName = productName.replace(/\s/g, '');
+
+module.exports = {
+  appId,
+  productName,
+  buildVersion: version,
+  files: [
+    '!assets/*',
+    '!scripts/*',
+    '!test/*',
+    '!.*'
+  ],
+  mac: {
+    icon,
+    target: {
+      target: 'default',
+      arch: [
+        'x64',
+        'arm64'
+      ],
+    },
+    darkModeSupport: true
+  },
+  dmg: {
+    icon,
+    artifactName: `${fileName}-v\${version}-MacOS-\${arch}.\${ext}`
+  },
+  win: {
+    icon,
+    target: [
+      'nsis',
+      'portable'
+    ]
+  },
+  nsis: {
+    artifactName: `${fileName}-v\${version}-Windows-setup.\${ext}`
+  },
+  portable: {
+    artifactName: `${fileName}-v\${version}-Windows-portable.\${ext}`
+  },
+  linux: {
+    icon,
+    target: [
+      'AppImage'
+    ],
+    executableName: productName,
+    category: 'Network'
+  },
+  appImage: {
+    artifactName: `${fileName}-v\${version}-Linux.\${ext}`
+  }
+};
